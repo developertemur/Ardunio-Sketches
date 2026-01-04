400fcc8fdadecfc1151cca761217eabe71881a44
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2026-01-04 11:07:32 +0500

    Initial

---
+++ IRdecoder/IRdecoder.ino
@@ -0,0 +1,20 @@
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 2
+
+void setup()
+{
+  
+  Serial.begin(115200); // // Establish serial communication
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+
+
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
